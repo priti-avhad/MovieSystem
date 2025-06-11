8919f9e3e7f33d629fff0ccc248f1e17edd3de24
akash-sawant <akashsawant1414@gmail.com>
2025-06-11 14:32:24 +0530

    home.ejs file upload

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 let express=require("express");
 let app=express();
 let bodyparser=require("body-parser");
+
 let session=require("express-session");
 
 
-// let router=require("../src/routes/regrouts.js");
 let conn=require("../src/config/db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use("/",router);
 
 app.set('view engine','ejs');
 
