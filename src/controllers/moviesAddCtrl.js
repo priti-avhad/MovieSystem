34c9c20f96b502460192a75f962410998643be0d
priti-avhad <pritiavhad68@gmail.com>
2025-06-14 18:00:43 +0530

    changes
    done

--- src/controllers/moviesAddCtrl.js
+++ src/controllers/moviesAddCtrl.js
@@ -1,29 +1,16 @@
-const conn = require("../config/db.js");
+const movieModel = require("../models/moviesAddModel");
 
 exports.addMove = (req, res) => {
-  const {
-    title, description, release_date, genre,
-    director, language, country, budget,
-    revenue, runtime, trailer_url
-  } = req.body;
+  const movieData = req.body;
 
   if (!req.file) {
     return res.status(400).send("Poster image is required");
   }
 
   const posterurl = "/images/" + req.file.filename;
+  console.log("movie url:", movieData.movieurl);
 
-  const sql = `
-    INSERT INTO movies 
-    (title, description, releasedate, genre, director, language, country, budget, revenue, runtime, posterurl, trailerurl)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-  `;
-
-  conn.query(sql, [
-    title, description, release_date, genre,
-    director, language, country, budget, revenue,
-    runtime, posterurl, trailer_url
-  ], (err, result) => {
+  movieModel.insertMovie(movieData, posterurl, (err, result) => {
     if (err) {
       console.error("Insert Error:", err);
       return res.status(500).send("Failed to save movie");
--- src/routes/moviesAddRoutes.js
+++ src/routes/moviesAddRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const movieCtrl = require("../controllers/moviesAddCtrl");
+const movieRoutes=require("../routes/moviesAddRoutes");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "public/images"),
@@ -10,6 +11,10 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+router.get("/add-movie", (req, res) => {
+  res.render("AddMovie"); // or whatever your view filename is
+});
+
 
 router.post("/add-movie", upload.single("poster"), movieCtrl.addMove);
 
